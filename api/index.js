cb8508664012026416f0f90d6a80f6b3ab0e062e
vy211 <vipinkumar.y@simplify3x.com>
2024-09-24 13:34:24 +0530

    some port

--- api/index.js
+++ api/index.js
@@ -40,6 +40,6 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
 });
-app.listen(4000, () => {
-  console.log("server is listening at port 4000.../");
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening at port ${process.env.PORT}...`);
 });
